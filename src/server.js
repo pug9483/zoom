f18e3c9e518cceafd47a66bb878d582149f9aaca
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-03-23 00:32:15 +0900

    WebSocket

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,6 +8,22 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+//Http 서버
+const server = http.createServer(app);
+
+//WebSocket 서버
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser✅");
+  socket.on("close", () => {
+    console.log("Disconnected from the Browser❌");
+  });
+  socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
