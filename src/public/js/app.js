5787553ddd54bc97f4bb026551e972110e1a5be6
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-03-23 01:08:08 +0900

    Chat using websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -5,9 +7,18 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("Just got this: ", message.data, "from the server");
+  console.log("New message: ", message.data);
 });
 
 socket.addEventListener("close", () => {
-  console.log("Connected to Server❌");
+  console.log("Disconnected to Server❌");
 });
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -10,20 +10,25 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 //Http 서버
 const server = http.createServer(app);
 
 //WebSocket 서버
 const wss = new WebSocket.Server({ server });
 
+// 누군가가 우리 서버에 연결하면, connection저장 -> 받은 메시지를 다른 모든 socket에게 전달 가능하다.
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser✅");
   socket.on("close", () => {
     console.log("Disconnected from the Browser❌");
   });
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+  });
   socket.send("hello!!");
 });
 
-server.listen(3000, handleListen);
+server.listen(3000, () => console.log(`Listening on http://localhost:3000`));
